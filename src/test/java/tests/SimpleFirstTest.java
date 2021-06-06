7b36e06e38426a0ed558ff3e727b6e08925551db
plmaskw2 <maciek115606@gmail.comn>
2021-06-06 04:36:41 +0200

    Test updated

--- src/test/java/tests/SimpleFirstTest.java
+++ src/test/java/tests/SimpleFirstTest.java
@@ -4,9 +4,12 @@ package tests;
 import Configuration.Configuration;
 import Configuration.Driver;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class SimpleFirstTest {
 
@@ -19,11 +22,15 @@ public class SimpleFirstTest {
 
     @Test
     public void test1(){
+        WebElement AGREE = driver.findElement(By.xpath("//div[text()='Zgadzam się']"));
+        AGREE.click();
 
+        WebElement GOOGLE_SEARCH = driver.findElement(By.xpath("//input[@type='submit']"));
+        Assert.assertEquals("Szukaj w Google", GOOGLE_SEARCH.getAttribute("value"));
     }
 
     @After
     public void tearDown(){
-
+        driver.quit();
     }
 }
